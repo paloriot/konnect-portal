1a74a2976ac6bf7eb3541c4d119ad13b401fec52
Pierre-Alexandre <102515343+paloriot@users.noreply.github.com>
2023-08-30 11:09:19 +0200

    Update index.ts
    add local 'he'

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -5,12 +5,14 @@ import { de } from './de'
 import { en } from './en'
 import { es_ES } from './es_ES'
 import { fr } from './fr'
+import { he } from './he'
 
 // If you wish to define additional locales, add them here separated by `|`
 export type DefinedLocales = |
 'ca_ES' |
 'de' |
 'en' |
+'he' |
 'es_ES' |
 'fr'
 
@@ -18,6 +20,7 @@ export const locales: Record<DefinedLocales, I18nType> = {
   ca_ES,
   de,
   en,
+  he,
   es_ES,
   fr
 }
